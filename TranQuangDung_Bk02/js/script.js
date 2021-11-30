64aab7b6460434c960c66e3d537f055b8922a559
quangdung07 <82128909+quangdung07@users.noreply.github.com>
2021-11-30 10:16:20 +0700

    Signed-off-by: quangdung07 <82128909+quangdung07@users.noreply.github.com>

--- TranQuangDung_Bk02/js/script.js
+++ TranQuangDung_Bk02/js/script.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
-	$('.noi-dung').slideUp(); /*ẩn nội dung*/
+	$('.noidung').slideUp();
 
 	$('.khoi h3').on('click', function(event) {
-		// ngăn chặn mặc định
 		event.preventDefault();
-		// đóng/ mở
-		// $('.noi-dung').slideUp(); /*ẩn nội dung*/
 		$(this).next().slideToggle();
 
-		// $(this).offset().top lấy vị trí của phần tử this
-
 		$('html, body').animate({scrollTop:$(this).offset().top},400); 
-	});
-
-
-	// quay về đầu trang 
-	$('#btn-top').click(function(event) {
-		/* Act on the event */
-		console.log('click');
-		$('html, body').animate({scrollTop:0},400);
 	});
 });
